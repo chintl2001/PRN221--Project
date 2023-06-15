bed337ec28f370b2627f0ae4977eb0d29116aa91
quynhbn272 <115777816+quynhbn27@users.noreply.github.com>
2023-06-15 07:58:05 +0700

    Fix

--- Final_Project/Final_Project/Pages/Admin/AddBlog.cshtml.cs
+++ Final_Project/Final_Project/Pages/Admin/AddBlog.cshtml.cs
@@ -1,3 +1,4 @@
+﻿using Final_Project.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,49 @@ namespace Final_Project.Pages.Admin
 {
     public class AddBlogModel : PageModel
     {
+        private COFFEEContext COFFEEContext;
+        [BindProperty]
+        public string title { get; set; }
+        [BindProperty]
+        public string image { get; set; }
+        [BindProperty]
+        public string content { get; set; }
+        [BindProperty]
+        public string shortContent { get; set; }
+        [BindProperty]
+        public string auther { get; set; }
+        [BindProperty]
+        public DateTimeOffset publishDate { get; set; }
+
+        public AddBlogModel(COFFEEContext _COFFEEContext)
+        {
+            COFFEEContext = _COFFEEContext;
+        }
         public void OnGet()
         {
+
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                Blog addBlog = new Blog
+                {
+                    Title = title,
+                    Image = image,
+                    Content = content,
+                    ShortContent = shortContent,
+                    Author = auther,
+                    PublishDate = TimeZoneInfo.ConvertTimeToUtc(publishDate.LocalDateTime, TimeZoneInfo.Local)
+                };
+
+                COFFEEContext.Blogs.Add(addBlog);
+                await COFFEEContext.SaveChangesAsync();
+
+                return RedirectToPage("/Admin/BlogList");
+            }
+
+            return Page();
         }
     }
 }
