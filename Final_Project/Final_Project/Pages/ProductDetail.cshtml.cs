2d5995bc46045d89040062dd8c535ef938eeec42
quynhbn272 <115777816+quynhbn27@users.noreply.github.com>
2023-07-25 02:50:23 +0700

    Done Comment, Done Product Detail

--- Final_Project/Final_Project/Pages/ProductDetail.cshtml.cs
+++ Final_Project/Final_Project/Pages/ProductDetail.cshtml.cs
@@ -1,12 +1,32 @@
+using Final_Project.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Final_Project.Pages.Shared
 {
     public class ProductDetailModel : PageModel
     {
-        public void OnGet()
+        private readonly COFFEEContext _coffeeContext;
+
+        public Product Product { get; set; }
+        public List<Blog> Blogs { get; set; }
+        public ProductDetailModel(COFFEEContext coffeeContext)
         {
+            _coffeeContext = coffeeContext;
+        }
+        public string Username;
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Product = await _coffeeContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+            Blogs = _coffeeContext.Blogs.OrderBy(p => Guid.NewGuid()).Take(2).ToList();
+            if (Product == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
         }
     }
 }
